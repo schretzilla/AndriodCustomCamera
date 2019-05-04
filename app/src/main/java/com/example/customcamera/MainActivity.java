9ee399c7faeaa38e062d0cd6bfe16c47eed53b9b
schretzilla <schretzilla@gmail.com>
2019-05-03 21:21:14 -0700

    Added first crack at video recording
    * Added Record button
    * Button starts and stops video recording
    *  Added audio and camera hardware components to required permissions
    * Updated git ignore to ignore ./idea/vcs.xml file

--- app/src/main/java/com/example/customcamera/MainActivity.java
+++ app/src/main/java/com/example/customcamera/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.customcamera;
 
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import java.io.File;
@@ -12,15 +17,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 public class MainActivity extends AppCompatActivity {
 
     Camera camera;
     FrameLayout frameLayout;
-    ShowCamera showCamera;
+    ShowCamera cameraPreview;
+
+    //Log tag name
+    private static final String TAG = "Main Activity";
+
+    //bool for if video is recording
+    private boolean isRecording = false;
 
     //Home directory to store all files in
     File birdWatchHomeDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
@@ -32,11 +46,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
 
+        //open and start camera
         camera = getCameraInstance();
 
         //show camera view
-        showCamera = new ShowCamera(this,camera);
-        frameLayout.addView(showCamera);
+        cameraPreview = new ShowCamera(this,camera);
+        frameLayout.addView(cameraPreview);
 
         //create birdwatch home directory if it doesn't exist
         if(!birdWatchHomeDir.exists())
@@ -128,5 +143,151 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Event for capturing video
+    public void captureVideo(View v)
+    {
+        if (isRecording) {
+            // stop recording and release camera
+            mediaRecorder.stop();  // stop the recording
+            releaseMediaRecorder(); // release the MediaRecorder object
+            camera.lock();         // take camera access back from MediaRecorder
 
+            // inform the user that recording has stopped
+            setCaptureButtonText("Capture");
+            isRecording = false;
+        } else {
+            // initialize video camera
+            if (prepareVideoRecorder()) {
+                // Camera is available and unlocked, MediaRecorder is prepared,
+                // now you can start recording
+                mediaRecorder.start();
+
+                // inform the user that recording has started
+                setCaptureButtonText("Stop");
+                isRecording = true;
+            } else {
+                // prepare didn't work, release the camera
+                releaseMediaRecorder();
+                // inform user
+            }
+        }
+    }
+
+    private void setCaptureButtonText(String text)
+    {
+       Button recordButton = (Button) findViewById(R.id.recordVideo);
+       recordButton.setText(text);
+    }
+
+    MediaRecorder mediaRecorder;
+
+    //Handles locking the camera, configuring and preparing the media recorder instance
+    private boolean prepareVideoRecorder(){
+
+        camera = getCameraInstance();
+        mediaRecorder = new MediaRecorder();
+
+        // Step 1: Unlock and set camera to MediaRecorder
+        camera.unlock();
+        mediaRecorder.setCamera(camera);
+
+        // Step 2: Set sources
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        // Step 4: Set output file
+        String outputfile = getOutputMediaFile(MEDIA_TYPE_VIDEO).toString();
+        mediaRecorder.setOutputFile(outputfile);
+
+        // Step 5: Set the preview output
+        mediaRecorder.setPreviewDisplay(cameraPreview.getHolder().getSurface());
+
+        // Step 6: Prepare configured MediaRecorder
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+
+    //Releasing video camera methods
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseMediaRecorder(){
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();   // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = null;
+            camera.lock();           // lock camera for later use
+        }
+    }
+
+    private void releaseCamera(){
+        if (camera != null){
+            camera.release();        // release the camera for other applications
+
+            //camera = null; //TODO Do we need to set camera to null?
+        }
+    }
+
+    //Methods for Saving video from Camera
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), "CustomCamera");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
 }
